949abea373ebeffa9e008e04d98d4b802b47a972
koashima <48111257+koashima@users.noreply.github.com>
2020-05-26 14:16:00 -0700

    Finish setting up server.js

--- server.js
+++ server.js
@@ -17,3 +17,12 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 // The following "catch all" route (note the *)is necessary
 // for a SPA's client-side routing to properly work
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', index.html));
+});
+
+// Configure to use port 3001 instead of 3000 during
+// development to avoid collision with React's dev server
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => console.log(`Express app running on port ${port}`));
