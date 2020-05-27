8fcd6f137d13dfd5a8cef53f7cec3ca7c7d8a2db
koashima <48111257+koashima@users.noreply.github.com>
2020-05-27 15:14:44 -0700

    adds simple header and starts on setting up Router components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    
-    <div className="App">
-      <header className="App-header" style={ {color: "" }}>
-        recrue
-      </header>
-      
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header" style={{ fontStyle: "italic"}}>R E <span style={{ textDecoration: 'line-through'}}>C R U E</span></header>
+        <Nav/>
+      </div>      
+    </Router>
+
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = (props) => { 
+  return ( 
+    <div>
+      <Link>
+      
+      </Link>
+    </div>
+  )
+}
+
+export default Nav;
