758829bd1909a46d037165cba9db6b1faa790c60
koashima <48111257+koashima@users.noreply.github.com>
2020-05-29 11:25:08 -0700

    Cleans up db and adds state to SignupForm

--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -1,7 +1,15 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignupForm = (props) => { 
+  
+  const [userCred, setUserCred] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConf: ""
+  });
+  
   return (
     <h1>SIGNUP</h1>
   )
